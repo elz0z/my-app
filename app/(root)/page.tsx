3e70e03b8bb81504afd11b5331023129c2853e53
elz0z <ghost.lover0@yahoo.com>
2025-06-14 06:28:45 +0300

    edit global.css to manage tailwind utilities

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
 	return (
 		<>
-			<h1 className= 'text-2xl'  >Home Page</h1>
+			<h1 className= 'heading'  >Home Page</h1>
 		</>
 	);
 }
